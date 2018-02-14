2f393c9129ce2e2378c0e26264e15e9b8b95db45
KADOC-PC\antoine <aganthie@gmail.com>
2018-02-14 10:17:21 +0100

    Login OK - hors ligne et en ligne

--- Onek/Onek/MainPage.xaml.cs
+++ Onek/Onek/MainPage.xaml.cs
@@ -3,7 +3,10 @@ using Onek.utils;
 using Plugin.Connectivity;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -26,35 +29,54 @@ namespace Onek
 
         async void OnButtonLoginClicked(object sender, EventArgs e)
         {
-            /*if (CrossConnectivity.Current.IsConnected)
+            if (CrossConnectivity.Current.IsConnected)
             {
                 //ONLINE LOGIN
-
-                await DisplayAlert("Erreur", "Vous avez internet ! #future", "OK");
-                //return;
-            }*/
-            //else
-            //{
-
-                //await DisplayAlert("Erreur", "Vous avez internet ! #future", "OK");
-                //return;
-            //}
-            //else
-            //{
-
+                User user = null;
+                LoginManager loginManager = new LoginManager();
+                if (LoginEntry.Text != null && PasswordEntry != null)
+                {
+                    //Send login request
+                    loginManager.Login = LoginEntry.Text;
+                    loginManager.Password = PasswordEntry.Text;
+                    String loginJson = loginManager.GenerateLoginJson();
+                    HttpWebResponse httpWebResponse = loginManager.SendAuthenticationRequest(loginJson);
+                    //Check login response
+                    if (httpWebResponse != null && httpWebResponse.StatusCode.Equals(HttpStatusCode.OK))
+                    {
+                        //Get user json account
+                        Stream responseStream = httpWebResponse.GetResponseStream();
+                        StreamReader streamReader = new StreamReader(responseStream, Encoding.UTF8);
+                        String jsonAccount = streamReader.ReadToEnd();
+                        List<User> users = JsonParser.DeserializeJsonAccount(jsonAccount);
+                        user = users.First();
+                        //Save user in local jsonAccount
+                        JsonParser.SaveJsonAccountInMemory(users);
+                        //Display event page
+                        await Navigation.PushAsync(new EventsPage(user));
+                        return;
+                    }
+                }
+            }
+            else
+            {
                 //OFFLINE LOGIN
                 List<User> logins = JsonParser.LoadLoginJson();
-
+                SHA1Managed sha1 = new SHA1Managed();
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(PasswordEntry.Text));
+                String hashedPassword = String.Join("", hash.Select(b => b.ToString("x2")).ToArray());
                 foreach (User u in logins)
                 {
                     if (LoginEntry.Text != null && PasswordEntry.Text != null
-                    && LoginEntry.Text.Equals(u.Login) && PasswordEntry.Text.Equals(u.Password))
+                    && LoginEntry.Text.Equals(u.Login) 
+                    && hashedPassword.Equals(u.Password))
                     {
-                        
+
                         await Navigation.PushAsync(new EventsPage(u));
                         return;
                     }
                 }
+            }
 
             await DisplayAlert("Erreur", "Le nom d'utilisateur ou le mot de passe est erroné", "OK");
         }
---
+++ Onek/Onek/data/LoginManager.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using Onek.utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Onek.data
+{
+    class LoginManager
+    {
+        public String Login { get; set; }
+        public String Password { get; set; }
+
+        public String GenerateLoginJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        public HttpWebResponse SendAuthenticationRequest(String jsonLogin)
+        {
+            try
+            {
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(ApplicationConstants.serverLoginURL);
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+                StreamWriter streamWriter = new StreamWriter(httpWebRequest.GetRequestStream());
+                streamWriter.Write(jsonLogin);
+                streamWriter.Flush();
+                streamWriter.Close();
+                return (HttpWebResponse)httpWebRequest.GetResponse();
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+    }
+}
