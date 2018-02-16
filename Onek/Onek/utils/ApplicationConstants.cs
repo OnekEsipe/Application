372a452ffe161d5882f558ba2a5906aae67326b7
KADOC-PC\antoine <aganthie@gmail.com>
2018-02-16 11:57:26 +0100

    Url en mode démo

--- Onek/Onek/utils/ApplicationConstants.cs
+++ Onek/Onek/utils/ApplicationConstants.cs
@@ -10,9 +10,9 @@ namespace Onek.utils
         public static String pathToPersonnalFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
         public static String pathToJsonAccountFile = Path.Combine(pathToPersonnalFolder, "account.json"); 
         public static String jsonDataDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-        public static String serverEventURL = "https://173.249.25.49/serveur/api/app/events/[id_event]/[login_user]/export";
-        public static String serverLoginURL = "https://173.249.25.49/serveur/api/app/login";
-        public static String serverEvaluationURL = "https://173.249.25.49/serveur/api/app/evaluation";
+        public static String serverEventURL = "https://173.249.25.49/demo/api/app/events/[id_event]/[login_user]/export";
+        public static String serverLoginURL = "https://173.249.25.49/demo/api/app/login";
+        public static String serverEvaluationURL = "https://173.249.25.49/demo/api/app/evaluation";
         public static String pathToJsonToSend = Path.Combine(jsonDataDirectory, "ToSend");
     }
 }
