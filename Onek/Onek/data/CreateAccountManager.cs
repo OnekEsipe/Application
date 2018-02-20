fbf7e26bdfe933a65059f7652b87d9dbded27421
KADOC-PC\antoine <aganthie@gmail.com>
2018-02-20 10:44:33 +0100

    Création du compte jury OK

---
+++ Onek/Onek/data/CreateAccountManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Onek.data
+{
+    class CreateAccountManager
+    {
+        public String Login { get; set; }
+        public String Lastname { get; set; }
+        public String Firstname { get; set; }
+        public String Mail { get; set; }
+        public String Password { get; set; }
+
+        /// <summary>
+        /// Check if password contains at least 6 characters and at least one uppercase letter
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static Boolean CheckPassword(String password)
+        {
+            if (password.Length < 6 || new Regex("[A-Z]").Matches(password).Count < 1)
+                return false;
+            return true;
+            
+        }
+    }
+}
