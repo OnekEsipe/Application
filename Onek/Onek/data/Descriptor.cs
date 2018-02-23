73e071417bc32ecc95c04c35bee26aee6619213b
Axel ROLO <axel.rolo1993@gmail.com>
2018-02-23 15:09:11 +0100

    Adapt X, Y and Size of Button Notation for Tablet and Phone

--- Onek/Onek/data/Descriptor.cs
+++ Onek/Onek/data/Descriptor.cs
@@ -8,33 +8,9 @@ namespace Onek.data
     {
         public String Text { get; set; }
         public String Level { get; set; }
-        private Color backgroundColor = Color.LightBlue;
-        private Color textColor = Color.Black;
 
         public event PropertyChangedEventHandler PropertyChanged;
-
-        public Color BackgroundColor
-        {
-            get { return backgroundColor; }
-            set
-            {
-                
-                backgroundColor = value;
-                OnPropertyChanged("BackgroundColor");
-            }
-        }
-
-        public Color TextColor
-        {
-            get { return textColor; }
-            set
-            {
-
-                textColor = value;
-                OnPropertyChanged("TextColor");
-            }
-        }
-
+        
         protected void OnPropertyChanged(PropertyChangedEventArgs e)
         {
             PropertyChangedEventHandler handler = PropertyChanged;
