a7bcbe382746fcce1b9cd457c56d7a464ac3bf7d
Axel ROLO <axel.rolo1993@gmail.com>
2018-03-06 18:57:09 +0100

    commit 1

--- Onek/Onek/NotationPage.xaml.cs
+++ Onek/Onek/NotationPage.xaml.cs
@@ -182,6 +182,7 @@ namespace Onek
         /// <summary>
         /// Event thrown when "Enregistrer" button is touched
         /// Save the evaluation and come back to overview
+        /// Useless
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -198,16 +199,22 @@ namespace Onek
         /// </summary>
         void SaveCriteria()
         {
-            CurrentCriteria.SelectedDescriptor = SelectedDescripteur;
-            if (SelectedDescripteur != null)
+            if (SelectedDescripteur != null && !SelectedDescripteur.Equals(CurrentCriteria.SelectedDescriptor))
             {
-                CurrentCriteria.SelectedLevel = SelectedDescripteur.Level;
+                CurrentCriteria.SelectedDescriptor = SelectedDescripteur;
+                if (SelectedDescripteur != null)
+                {
+                    CurrentCriteria.SelectedLevel = SelectedDescripteur.Level;
+                }
+                else
+                {
+                    CurrentCriteria.SelectedLevel = "";
+                } 
             }
-            else
+            if (Comment != null && !(Comment.Equals(CurrentCriteria.Comment)))
             {
-                CurrentCriteria.SelectedLevel = "";
+                CurrentCriteria.Comment = Comment;
             }
-            CurrentCriteria.Comment = Comment;
         }
 
         /// <summary>
@@ -219,6 +226,12 @@ namespace Onek
         void OnButtonRetourClicked(object sender, EventArgs e)
         {
             Navigation.PopAsync();
+        }
+
+        protected override void OnDisappearing()
+        {
+            SaveCriteria();
+            base.OnDisappearing();
         }
 
         /// <summary>
